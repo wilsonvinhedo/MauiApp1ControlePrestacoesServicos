84440d565bbab68e5d82c20671d0930ec89628b9
Wilson A C Lima <wilsonvinhedo@outlook.com>
2025-04-14 19:17:02 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1ControlePrestacoesServicos/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp1ControlePrestacoesServicos
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
