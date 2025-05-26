58656706ad763c3e61040df9c0563ee66a474c7b
Wilson A C Lima <wilsonvinhedo@outlook.com>
2025-05-26 10:55:00 -0300

    Atualização de maio 26-05 muitos bugs para corrigir

--- MauiApp1ControlePrestacoesServicos/AppShell.xaml.cs
+++ MauiApp1ControlePrestacoesServicos/AppShell.xaml.cs
@@ -1,10 +1,23 @@
-﻿namespace MauiApp1ControlePrestacoesServicos
+﻿using Microsoft.Extensions.DependencyInjection;
+using MauiApp1ControlePrestacoesServicos.Database;
+
+namespace MauiApp1ControlePrestacoesServicos;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    private readonly DatabaseHelper _database;
+
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        _database = MauiProgram.CreateMauiApp().Services.GetService<DatabaseHelper>();
+        InitializeComponent();
+        Routing.RegisterRoute(nameof(ClientesPage), typeof(ClientesPage));
+        Routing.RegisterRoute(nameof(ServicosPage), typeof(ServicosPage));
+        Routing.RegisterRoute(nameof(AgendamentosPage), typeof(AgendamentosPage));
+        Routing.RegisterRoute(nameof(FinanceiroPage), typeof(FinanceiroPage));
+        Routing.RegisterRoute(nameof(RelatorioPage), typeof(RelatorioPage));
+
+        // Define a página inicial
+        GoToAsync("//ClientesPage");
     }
 }
---
+++ MauiApp1ControlePrestacoesServicos/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using MauiApp1ControlePrestacoesServicos.Database;
+
+namespace MauiApp1ControlePrestacoesServicos;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // Registrar o DatabaseHelper como um serviço singleton
+        builder.Services.AddSingleton<DatabaseHelper>();
+
+        // Registrar o AppShell como a página inicial
+        builder.Services.AddSingleton<Shell>(s => new AppShell());
+
+        return builder.Build();
+    }
+}
