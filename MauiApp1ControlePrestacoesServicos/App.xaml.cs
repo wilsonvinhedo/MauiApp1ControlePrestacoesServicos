47e0ff9484c428991509ee4cc9f9fa1188512617
Wilson A C Lima <wilsonvinhedo@outlook.com>
2025-05-29 15:39:38 -0300

    atualização

--- MauiApp1ControlePrestacoesServicos/App.xaml.cs
+++ MauiApp1ControlePrestacoesServicos/App.xaml.cs
@@ -7,15 +7,9 @@ public partial class App : Application
     private static DatabaseHelper? _database;
     public static DatabaseHelper Database => _database ??= new DatabaseHelper();
 
-    [Obsolete]
     public App()
     {
-        InitializeComponent();
+        InitializeComponent(); // Este método é gerado automaticamente
         MainPage = new AppShell();
-    }
-
-    private void InitializeComponent()
-    {
-        throw new NotImplementedException();
     }
 }
---
+++ MauiApp1ControlePrestacoesServicos/Converters/SaldoColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace MauiApp1ControlePrestacoesServicos.Converters
+{
+    public class SaldoColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal saldo)
+            {
+                // Retorna verde para valores positivos, vermelho para negativos
+                return saldo >= 0 ? Colors.Green : Colors.Red;
+            }
+
+            // Cor padrão se o valor não for decimal
+            return Colors.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Não necessário para conversão one-way (apresentação)
+            throw new NotImplementedException();
+        }
+    }
+}
