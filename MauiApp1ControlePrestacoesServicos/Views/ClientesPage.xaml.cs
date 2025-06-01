ada6e9699d316b66277e0de77ee0d1dfe827d646
Wilson A C Lima <wilsonvinhedo@outlook.com>
2025-06-01 16:28:55 -0300

    Icones 01-06-25

--- MauiApp1ControlePrestacoesServicos/Views/ClientesPage.xaml.cs
+++ MauiApp1ControlePrestacoesServicos/Views/ClientesPage.xaml.cs
@@ -1,24 +1,22 @@
 ﻿using MauiApp1ControlePrestacoesServicos.Models;
-using MauiApp1ControlePrestacoesServicos.ViewModels;
 
 namespace MauiApp1ControlePrestacoesServicos.Views;
 
 public partial class ClientesPage : ContentPage
 {
-    private readonly ClientesViewModel ViewModel;
-
-    public ClientesPage() // ✅ Construtor correto: não tem tipo de retorno!
+    public ClientesPage()
     {
         InitializeComponent();
-        ViewModel = new ClientesViewModel();
-        BindingContext = ViewModel;
     }
 
     private async void OnExcluirClicked(object sender, EventArgs e)
     {
         if (clientesCollection.SelectedItem is Cliente clienteSelecionado)
         {
-            await ViewModel.ExcluirCliente(clienteSelecionado);
+            if (BindingContext is ViewModels.ClientesViewModel vm)
+            {
+                await vm.ExcluirCliente(clienteSelecionado);
+            }
         }
     }
 }
